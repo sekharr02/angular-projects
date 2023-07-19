bda1030191f03661780d6d05675643bb07aef978
sekharr02 <learningnew351@gmail.com>
2023-07-19 10:39:01 +0530

    form group created

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-user.component.scss']
 })
 export class CreateUserComponent {
+
+  public formUser:FormGroup=new FormGroup({
+    name : new FormControl(),
+    age : new FormControl(),
+    phone: new FormControl(),
+    email:new FormControl()
+  })
+
+  submit(){
+    console.log(this.formUser)
+  }
 
 }
