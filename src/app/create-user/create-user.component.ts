a250672b5c1e03054383a4544724db7c82199000
sekharr02 <learningnew351@gmail.com>
2023-07-26 10:32:58 +0530

    custom validators created

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormArray, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
+import { domainValidator, stateValidator } from '../validator';
 
 @Component({
   selector: 'app-create-user',
@@ -12,10 +13,10 @@ export class CreateUserComponent {
     name : new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
     age : new FormControl('',[Validators.required,Validators.min(0),Validators.max(25)]),
     phone: new FormControl('',[Validators.required,Validators.min(1000000000),Validators.max(9999999999)]),
-    email:new FormControl('',[Validators.required,Validators.email]),
+    email:new FormControl('',[Validators.required,Validators.email,domainValidator]),
     address: new FormGroup({
       hno:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
-      state:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+      state:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(10),stateValidator]),
       pin:new FormControl('',[Validators.required,Validators.min(100000),Validators.max(999999)])
     }),
     type:new FormControl(),
